a2a4b8ad6962b83eaeb03f298d472239ca773148
Vivek Vishwakarma <karma.vishwakarma@gmail.com>
2021-11-06 12:30:02 +0530

    initial commit

---
+++ src/main/java/com/vivek/moviecatalogservice/resources/MovieCatalogoueResource.java
@@ -0,0 +1,22 @@
+package com.vivek.moviecatalogservice.resources;
+
+import com.vivek.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogoueResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        return Collections.singletonList(
+                new CatalogItem("Avengers","a supe star movies which saves the world",5)
+        );
+    }
+}
