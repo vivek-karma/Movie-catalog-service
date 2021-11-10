1505a867c227bfcd2bb961f0c80c46fdc878dfac
Vivek Vishwakarma <karma.vishwakarma@gmail.com>
2021-11-10 10:38:10 +0530

    models added to connect to other api's

---
+++ src/main/java/com/vivek/moviecatalogservice/models/Rating.java
@@ -0,0 +1,21 @@
+package com.vivek.moviecatalogservice.models;
+
+public class Rating {
+    int ratings;
+
+    public Rating() {
+    }
+
+    public Rating(int ratings) {
+        this.ratings = ratings;
+    }
+
+    public int getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(int ratings) {
+        this.ratings = ratings;
+    }
+
+}
--- src/main/java/com/vivek/moviecatalogservice/resources/MovieCatalogoueResource.java
+++ src/main/java/com/vivek/moviecatalogservice/resources/MovieCatalogoueResource.java
@@ -1,9 +1,15 @@
 package com.vivek.moviecatalogservice.resources;
 
 import com.vivek.moviecatalogservice.models.CatalogItem;
+import com.vivek.moviecatalogservice.models.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,11 +18,31 @@ import java.util.List;
 @RequestMapping("/catalog")
 public class MovieCatalogoueResource {
 
-    @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+    @Autowired
+    RestTemplate restTemplate;
 
-        return Collections.singletonList(
-                new CatalogItem("Avengers","a supe star movies which saves the world",5)
-        );
+    @Value("${api.movie.url}")
+    String movieUrl;
+
+    @Value("${api.movie.port}")
+    String movieUrlPort;
+
+    @RequestMapping("/{userId}")
+    public ResponseEntity<String> getCatalog(@PathVariable("userId") String userId){
+
+        String fullUrl = movieUrl+":"+movieUrlPort+"/movie/"+userId;
+
+        Movie movie = null;
+        try {
+            movie = restTemplate.getForObject(fullUrl,Movie.class);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<String>("error", HttpStatus.BAD_REQUEST);
+        }
+
+//        return Collections.singletonList(
+//                new CatalogItem("Avengers","a supe star movies which saves the world",5)
+//        );
+        return new ResponseEntity<String>(movie.getMovieName(),HttpStatus.OK);
     }
 }
